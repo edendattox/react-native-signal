11f71ed067da160699d806d3c05e3dd67ee0c062
rahul kathait <edendattox@gmail.com>
2021-02-27 16:44:25 +0530

    added login and register screens

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCxIJeD0OmEQ2fc64Ig_FMsii4s5VMalTc",
+//     authDomain: "react-native-signal.firebaseapp.com",
+//     projectId: "react-native-signal",
+//     storageBucket: "react-native-signal.appspot.com",
+//     messagingSenderId: "531690053242",
+//     appId: "1:531690053242:web:48afc2e2cd5a16316ce5c3",
+//     measurementId: "G-39EFVH5B3P"
+//   };
